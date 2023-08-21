440aa8db0e3e5b2c5b07ef96a04cf31ead23f732
PERM KUMAR <83695512+Premkumar9799817360@users.noreply.github.com>
2023-08-21 14:30:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as  plt
+from matplotlib import pyplot as plt
 import plotly.express as px
 
 st.set_page_config(page_title="Data Analysis", page_icon=":bar_chart:", layout="wide")
